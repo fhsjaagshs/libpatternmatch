3014e8dac075940a8fb53cd607efd5121e6ef227
Nathaniel Symer <nate@natesymer.com>
2015-02-02 20:01:56 -0500

    Removed unnecessary code

--- pattern.cpp
+++ pattern.cpp
@@ -4,105 +4,16 @@
 // TODO: New wildcard: & matches 1 character
 
 namespace LibPM {
-  bool pattern::operator==(const string &rhs) const {
-    return matches(rhs);
-  }
+  bool pattern::operator==(const string &rhs) const { return matches(rhs); }
   
-  bool pattern::operator!=(const string &rhs) const {
-    return !(*this == rhs);
-  }
+  bool pattern::operator!=(const string &rhs) const { return !(*this == rhs); }
   
-  bool pattern::is_dynamic() const {
-    return (_index.size() > 0);
-  }
+  bool pattern::is_dynamic() const { return (_index.size() > 0); }
 
-  bool pattern::matches(string comp) const {
+  bool pattern::matches(string cppstr) const {
     if (_pattern[0] == ':') return true;
-    if (_pattern == comp) return true;
-    return _ptrncmp((char *)_pattern.c_str(), (char *)comp.c_str());
-  }
-  
-  /**/
-  
-  // all-in-one determine if a string matches while extracting the splats & wildcards
-  // Advantages:
-  //  One iteration over input string & pattern
-  // Disadvantages:
-  //  Cannot cache location of splats & wildcards, requiring much more processing
-  // bool pattern::match(string cppstr, list<string> &splats, map<string,string> &wildcards) {
-  //   if (_pattern[0] == ':') {
-  //     map<string, string> m;
-  //     m[_pattern.substr(1, _pattern.length()-1)] = cppstr;
-  //     return true;
-  //   }
-  //
-  //   char *str = (char *)cppstr.c_str();
-  //   char *pattern = (char *)_pattern.c_str();
-  //
-  //   while (*pattern != '\0') {
-  //     _advance_to_wildcard(pattern);
-  //     if (*pattern == '*') {
-  //
-  //     }
-  //   }
-  //
-  // }
-  
-   map<string, string> pattern::match_wildcards(string cppstr) const  {
-     map<string,string> wildcards;
-     char *pattern = (char *)_pattern.c_str();
-     char *str = (char *)cppstr.c_str();
-     int index_delta = 0;
-
-     for (auto i = _index.begin(); i != _index.end(); i++) {
-       unsigned idx = i->first;
-       string token = i->second;
-      
-       char *succ = pattern+idx+token.length();
-       char *vptr = str+idx+index_delta;
-       char *tmpv = vptr;
-       unsigned succlen = (i == _index.end()) ? 0 : next(i)->first-(idx+token.length());
-       unsigned vlen = _advance_to_str(tmpv, succ, succlen);
-      
-       index_delta += vlen;
-      
-       if (token[0] == '<' && token[token.length()-1] == '>') {
-         wildcards[token.substr(1, token.length()-2)] = string(vptr, vlen);
-         index_delta -= 1;
-       } else {
-         index_delta -= token.length();
-       }
-     }
-     return wildcards;
-   }
-
-  // map<string, string> pattern::match_wildcards(string comp) const {
-  //   if (_pattern[0] == ':') {
-  //     map<string, string> m;
-  //     m[_pattern.substr(1, _pattern.length()-1)] = comp;
-  //     return m;
-  //   }
-  //
-  //   map<string, string> m;
-  //   char *pattern = (char *)_pattern.c_str();
-  //   char *str = (char *)comp.c_str();
-  //
-  //   for (list<unsigned>::const_iterator i = _wildcard_indeces.begin(); i != _wildcard_indeces.end(); ++i) {
-  //     char *ptr = pattern+*i;
-  //     string name(ptr+1,_advance_to_succ(ptr)-2);
-  //
-  //     char *vptr = str+*i;
-  //     string value(vptr,_advance_to_str(vptr, ptr, _advance_to_char(ptr,'<')));
-  //
-  //     m[name] = value;
-  //     str += value.length()-(name.length()+2);
-  //   }
-  //
-  //   return m;
-  // }
-  
-  list<string> pattern::match_splats(string cppstr) const {
-    list<string> splats;
+    if (_pattern == cppstr) return true;
+    
     char *pattern = (char *)_pattern.c_str();
     char *str = (char *)cppstr.c_str();
     int index_delta = 0;
@@ -111,103 +22,101 @@ namespace LibPM {
       unsigned idx = i->first;
       string token = i->second;
       
-      char *succ = pattern+idx+token.length();
       char *vptr = str+idx+index_delta;
-      char *tmpv = vptr;
+      char *succ = pattern+idx+token.length();
       unsigned succlen = (i == _index.end()) ? 0 : next(i)->first-(idx+token.length());
-      unsigned vlen = _advance_to_str(tmpv, succ, succlen);
-      
+      unsigned vlen = _advance_to_str(vptr, succ, succlen);
+      if (strncmp(succ, vptr, succlen) != 0) return false;
+
       index_delta += vlen;
+      index_delta -= token.length();
+    }
+    
+    return true;
+  }
+  
+  map<string, string> pattern::match_wildcards(string cppstr) const  {
+    map<string,string> wildcards;
+    char *pattern = (char *)_pattern.c_str();
+    char *str = (char *)cppstr.c_str();
+    int index_delta = 0;
+
+    for (auto i = _index.begin(); i != _index.end(); i++) {
+      unsigned idx = i->first;
+      string token = i->second;
       
-      if (token[0] == '*') {
-        splats.push_back(string(vptr, vlen));
-        index_delta -= 1;
-      } else {
-        index_delta -= token.length();
+      char *vptr = str+idx+index_delta;
+      char *succ = pattern+idx+token.length();
+      unsigned succlen = (i == _index.end()) ? 0 : next(i)->first-(idx+token.length());
+      string value(vptr, _advance_to_str(vptr, succ, succlen));
+      
+      index_delta += value.length();
+      index_delta -= token.length();
+            
+      if (token[0] == '<' && token[token.length()-1] == '>') {
+        wildcards[token.substr(1, token.length()-2)] = value;
       }
     }
-    return splats;
+     
+    return wildcards;
   }
-
-  void pattern::create(string ptrn) {
-    _pattern = ptrn;
-    char *cptrn = (char *)_pattern.c_str();
-    _index = _gen_indeces(cptrn,cptrn);
-  }
+   
+   list<string> pattern::match_splats(string cppstr) const {
+     list<string> splats;
+     char *pattern = (char *)_pattern.c_str();
+     char *str = (char *)cppstr.c_str();
+     int index_delta = 0;
+          
+     for (auto i = _index.begin(); i != _index.end(); i++) {
+       unsigned idx = i->first;
+       string token = i->second;
+        
+       char *vptr = str+idx+index_delta;
+       char *succ = pattern+idx+token.length();
+       unsigned succlen = (i == _index.end()) ? 0 : next(i)->first-(idx+token.length());
+       string value(vptr, _advance_to_str(vptr, succ, succlen));
+      
+       index_delta += value.length();
+        
+       if (token[0] == '*') {
+         splats.push_back(value);
+         index_delta -= 1;
+       } else {
+         index_delta -= token.length();
+       }
+     }
+     return splats;
+   }
+   
+   void pattern::create(string ptrn) {
+     _pattern = ptrn;
+     char *cptrn = (char *)_pattern.c_str();
+     _index = _gen_indeces(cptrn,cptrn);
+   }
   
-  unsigned pattern::_advance_to_str(char *&ptr, char *str, unsigned n) const {
-    char *start = ptr;
-    while (*ptr != '\0' && (*str == '\0' || strncmp(ptr, str, n) != 0)) ptr++;
-    return (unsigned)(ptr-start);
-  }
+   unsigned pattern::_advance_to_str(char *&ptr, char *str, unsigned n) const {
+     char *start = ptr;
+     while (*ptr != '\0' && (*str == '\0' || strncmp(ptr, str, n) != 0)) ptr++;
+     return (unsigned)(ptr-start);
+   }
   
-  unsigned pattern::_advance_to_char(char *&ptr, char c) const {
-    char *start = ptr;
-    while (*ptr != '\0' && *ptr != c) ptr++;
-    return (unsigned)(ptr-start);
-  }
+   map <unsigned, string> pattern::_gen_indeces(char *str, char *ptr) const {
+     // advance to wildcard
+     while (*ptr != '\0' && *ptr != '*' && *ptr != '<') ptr++;
 
-  unsigned pattern::_advance_to_wildcard(char *&ptr) const {
-    char *start = ptr;
-    while (*ptr != '\0' && *ptr != '*' && *ptr != '<') ptr++;
-    return (unsigned)(ptr-start);
-  }
-
-  unsigned pattern::_advance_to_succ(char *&ptr) const {
-    char *start = ptr;
-    if (*ptr == '<') {
-      while (*ptr != '>') ptr++; ptr++;
-    } else if (*ptr == '*') {
-      ptr++;
-    }
-    return (unsigned)(ptr-start);
-  }
-
-  // todo: implement using cached indeces
-  bool pattern::_ptrncmp(char *pattern, char *comp) const {
-    if (strlen(pattern) == 0) return true;
-  
-    char *tmp = pattern;
-    if (_advance_to_succ(tmp) != 0) {
-      pattern = tmp;
-      char *succ = pattern;
-      unsigned succlen = _advance_to_wildcard(pattern);      
-      _advance_to_str(comp, succ, succlen);
-
-      if (strncmp(succ, comp, succlen) != 0) return false;
-      return _ptrncmp(pattern, comp+succlen);
-    } else {
-      if (pattern[0] != comp[0]) return false;
-      return _ptrncmp(pattern+1, comp+1);
-    }
-  }
-  
-  map <unsigned, string> pattern::_gen_indeces(char *str, char *ptr) const {
-    _advance_to_wildcard(ptr);
-    if (*ptr == '\0') return map <unsigned, string>();
-    unsigned idx = ptr-str;
-    unsigned token_len = _advance_to_succ(ptr);
-    
-    map <unsigned, string> recursive = _gen_indeces(str, ptr);
-    recursive[idx] = string(str+idx,token_len);
-    return recursive;
-  }
-  //
-  // list<unsigned> pattern::_get_splat_indeces_prime(char *str, char *ptr) const {
-  //   _advance_to_char(ptr, '*');
-  //   if (*ptr != '*') return list<unsigned>();
-  //
-  //   list<unsigned> recursive = _get_splat_indeces_prime(str, ptr+1);
-  //   recursive.push_front(ptr-str);
-  //   return recursive;
-  // }
-  //
-  // list<unsigned> pattern::_get_wildcard_indeces_prime(char *str, char *ptr) const {
-  //   _advance_to_char(ptr, '<');
-  //   if (*ptr == '\0') return list<unsigned>();
-  //
-  //   list<unsigned> recursive = _get_wildcard_indeces_prime(str, ptr+1);
-  //   recursive.push_front(ptr-str);
-  //   return recursive;
-  // }
+     if (*ptr == '\0') return map <unsigned, string>();
+     unsigned idx = ptr-str;
+     
+     char *start = ptr;
+     switch (*ptr) {
+       case '<': while (*ptr != '>') ptr++; ptr++; break;
+       case '*': ptr++; break;
+       default: break;
+     }
+     unsigned token_len = (unsigned)(ptr-start);
+     
+     map <unsigned, string> recursive = _gen_indeces(str, ptr);
+     recursive[idx] = string(str+idx,token_len);
+     return recursive;
+   }
 }
--- pattern.h
+++ pattern.h
@@ -11,7 +11,7 @@ namespace LibPM {
   
   class pattern {
   public:
-    pattern(const char *ptrn) { create(string(ptrn)); }
+    pattern(const char *ptrn) { create(ptrn); }
     pattern(string ptrn) { create(ptrn); }
     
     bool operator==(const string &rhs) const;
@@ -23,47 +23,20 @@ namespace LibPM {
     // check if a string matches the this pattern
     bool matches(string comp) const;
     
-    // extracts splats and wildcards from the string
-    // while checking if the string is a match.
-   // bool match(string cppstr, list<string> &splats, map<string,string> &wildcards);
+    // extracts all mappings from cppstr
+    map<string, string> match_wildcards(string cppstr) const;
     
-    map<string, string> match_wildcards(string comp) const;
+    // extracts all splats from cppstr
     list<string> match_splats(string cppstr) const;
-  
-  private:
+  protected:
     string _pattern;
     map<unsigned, string> _index;
     
-    // list<unsigned> _splat_indeces;
-    // list<unsigned> _wildcard_indeces;
-  
     void create(string ptrn);
-    
-    // recursive implementatino of match_splats()
-    list<string> _match_splats(char *str, char *pattern, list<string> &splats) const;
     
     // advance ptr to the next occurance of str
     unsigned _advance_to_str(char *&ptr, char *str, unsigned len) const;
     
-    // advance ptr to the next occurance of c
-    unsigned _advance_to_char(char *&ptr, char c) const;
-
-    // advance to first char of wildcard sequence
-    unsigned _advance_to_wildcard(char *&ptr) const;
-  
-    // advance ptr to the first char after a wildcard sequence
-    unsigned _advance_to_succ(char *&ptr) const;
-  
-    // compares a string to a pattern
-    bool _ptrncmp(char *pattern, char *comp) const;
-    
     map <unsigned, string> _gen_indeces(char *str, char *ptr) const;
-  
-    list<string> _get_splats(char *pattern, char *comp) const;
-    map<string, string> _get_wildcards(char *pattern, char *comp) const;
-    list<unsigned> _get_splat_indeces_prime(char *str, char *ptr) const; 
-    inline list<unsigned> _get_splat_indeces(char *str) const { return _get_splat_indeces_prime(str,str); }
-    list<unsigned> _get_wildcard_indeces_prime(char *str, char *ptr) const;
-    inline list<unsigned> _get_wildcard_indeces(char *str) const { return _get_wildcard_indeces_prime(str,str); }
   };
 }
